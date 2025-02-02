089591811eb289cde9a9433fc3a79368e216fb6c
girish <gsaundarkar@gmail.com>
2025-02-02 19:42:12 +0530

    types of exports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Functional from "./components/Functional";
 import Class from "./components/Class";
+import DefaultExport from "./components/DefaultExport";
+import { About, Contact } from "./components/NamedExport";
 
 function App() {
   return (
     <>
       <Functional />
       <Class />
+      <DefaultExport />
+      <About />
+      <Contact />
     </>
   );
 }
---
+++ src/components/NamedExport.js
@@ -0,0 +1,18 @@
+// Named Export
+
+function About() {
+    return (
+        <>
+            <h1>Named Exports :</h1>
+            <h1>1.About COmponent</h1>
+        </>
+    )
+}
+
+function Contact() {
+    return (
+        <h1>2.Contact COmponent</h1>
+    )
+}
+
+export { About, Contact };
